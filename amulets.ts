e5de5e8bfec799ece33a24fc628e5f027dffc482
PenguinSatanJr <penguinsatan107@gmail.com>
2026-09-01 19:59:41 +0400

    add default inventory

---
+++ amulets.ts
@@ -0,0 +1,23 @@
+export type Amulet = {
+    name: string;
+};
+
+const amuletOfStrength: Amulet = {
+    name: "Amulet of Strength",
+};
+
+const amuletOfKnowledge: Amulet = {
+    name: "Amulet of Knowledge",
+};
+
+const amuletOfDexterity: Amulet = {
+    name: "Amulet of Dexterity",
+};
+
+const scarabOfDefense: Amulet = {
+    name: "Scarab of Defense",
+};
+
+const vishantiCristal: Amulet = {
+    name: "Vishanti Cristal",
+};
---
+++ weapons.ts
@@ -0,0 +1,29 @@
+export type Weapon = {
+    name: string;
+    damage: number;
+};
+
+const longSword: Weapon = {
+    name: "Long Sword",
+    damage: 15,
+};
+
+const dagger: Weapon = {
+    name: "Dagger",
+    damage: 10,
+};
+
+const staff: Weapon = {
+    name: "Staff",
+    damage: 8,
+};
+
+const healingScroll: Weapon = {
+    name: "Healing Scroll",
+    damage: 1,
+};
+
+export const stick: Weapon = {
+    name: "Stick",
+    damage: 2,
+};
