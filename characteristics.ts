0eaca17e19b0f76db8af10904c0f4c0ed9d0eaf2
PenguinSatanJr <penguinsatan107@gmail.com>
2026-09-01 19:38:12 +0400

    add initial characteristics

---
+++ characteristics.ts
@@ -0,0 +1,33 @@
+type Characteristics = {
+    HP: number;
+    MP: number;
+    str: number;
+    dex: number;
+    int: number;
+    con: number;
+    speed: number;
+    pdef: number;
+    mdef: number;
+};
+
+const defaultHP = 50;
+const defaultMP = 0;
+const defaultStr = 10;
+const defaultDex = 10;
+const defaultInt = 10;
+const defaultCon = 10;
+const defaultSpeed = defaultDex * 5;
+const defaultPdef = 5;
+const defaultMdef = 5;
+
+const defaultCharacteristics: Characteristics = {
+    HP: defaultHP,
+    MP: defaultMP,
+    str: defaultStr,
+    dex: defaultDex,
+    int: defaultInt,
+    con: defaultCon,
+    speed: defaultSpeed,
+    pdef: defaultPdef,
+    mdef: defaultMdef,
+};
