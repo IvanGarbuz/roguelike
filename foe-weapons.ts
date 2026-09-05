8e4f74d0b6921bf3a0c1d9b3601bfa22d55fc078
younothing5 <superheroy2009@gmail.com>
2026-09-05 21:45:08 +0300

    add foe weapons

---
+++ foe-weapons.ts
@@ -0,0 +1,73 @@
+import { Weapon } from "./weapons";
+
+type FoeWeapon = Weapon
+
+const longSword: FoeWeapon = {
+    name: "Long Sword",
+    damage: 15,
+};
+
+const dagger: FoeWeapon = {
+    name: "Dagger",
+    damage: 10,
+};
+
+const staff: FoeWeapon = {
+    name: "Staff",
+    damage: 8,
+};
+
+const bow: FoeWeapon = {
+    name: "Bow",
+    damage: 12,
+};
+
+const claws: FoeWeapon = {
+    name: "Claws",
+    damage: 5,
+};
+
+const spear: FoeWeapon = {
+    name: "Spear",
+    damage: 14,
+};
+
+const fist: FoeWeapon = {
+    name: "Fist",
+    damage: 3,
+};
+
+const greatAxe: FoeWeapon = {
+    name: "Great Axe",
+    damage: 20,
+};
+
+const jaws: FoeWeapon = {
+    name: "Jaws",
+    damage: 7
+};
+
+const runicSword: FoeWeapon = {
+    name: "Runic Sword",
+    damage: 25
+};
+
+const greatClub: FoeWeapon = {
+    name: "Great Club",
+    damage: 18,
+};
+
+const tentacles: FoeWeapon = {
+    name: "Tentacles",
+    damage: 4,
+};
+
+const fangs: FoeWeapon = {
+    name: "Fangs",
+    damage: 5,
+};
+
+const necronomicon: FoeWeapon = {
+    name: "Necronomicon",
+    damage: 12,
+};
