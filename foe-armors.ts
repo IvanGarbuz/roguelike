a50c1ac85f9e886255a7efbc266d5c61a8600983
temcha111 <wwwrihydered@gmail.com>
2026-09-05 22:30:52 +0300

    add foe armor

---
+++ foe-armors.ts
@@ -0,0 +1,75 @@
+import { Armor } from "./armors";
+
+type FoeArmor = Armor;
+
+const rustyArmor: FoeArmor = {
+  name: "Rusty Armor",
+  pdef: 30,
+  mdef: 14
+};
+
+const rags: FoeArmor = {
+    name: "Rags",
+    pdef: 5,
+    mdef: 0
+}
+
+const naked: FoeArmor = {
+    name: "Naked",
+    pdef: 0,
+    mdef: 0
+}
+
+const tailcoat: FoeArmor = {
+    name: "Tailcoat",
+    pdef: 18,
+    mdef: 30
+}
+
+const hide: FoeArmor = {
+    name: "Hide",
+    pdef: 5,
+    mdef: 0
+}
+
+const furArmor: FoeArmor = {
+    name: "Fur Armor",
+    pdef: 17,
+    mdef: 5
+}
+
+const fullPlateArmor: FoeArmor = {
+    name: "Full Plate Armor",
+    pdef: 47,
+    mdef: 7
+}
+
+const robe: FoeArmor = {
+    name: "Robe",
+    pdef: 5,
+    mdef: 45    
+}
+
+const loincloth: FoeArmor = {
+    name: "Loincloth",
+    pdef: 1,
+    mdef: 0
+}
+
+const eboniteArmor: FoeArmor = {
+    name: "Ebonite Armor",
+    pdef: 60,
+    mdef: 20
+}
+
+const boneArmor: FoeArmor = {
+    name: "Bone Armor",
+    pdef: 35,
+    mdef: 10
+}
+
+const stoneArmor: FoeArmor = {
+    name: "Stone Armor",
+    pdef: 42,
+    mdef: 22
+}
