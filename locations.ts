5bae6974eb1ffd4773cdd10df60a3e723e4e32fd
temcha111 <wwwrihydered@gmail.com>
2026-09-19 19:16:35 +0300

    add location

---
+++ locations.ts
@@ -0,0 +1,15 @@
+type LocationSite = {
+name: string;
+}
+
+const dungeon: LocationSite = {
+name: "Dungeon"
+}
+
+const forest: LocationSite = {
+    name: "Forest"
+}
+
+const cursedCastle: LocationSite = {
+    name: "Cursed Castle"
+}
