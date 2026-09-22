501246cc0bd3b053f60a183a0cd1b518147c5e86
temcha111 <wwwrihydered@gmail.com>
2026-09-22 19:46:52 +0300

    add mage abilities

---
+++ abilities/mage-abilities.ts
@@ -0,0 +1,19 @@
+import { Ability } from "./warrior-abilities";
+
+const fireBall: Ability = {
+    name: "Fire Ball",
+    description: "The character attacks the opponent with a fireball",
+    damage: 29
+}
+
+const lightingBolt: Ability = {
+    name: "Lighting Bolt",
+    description: "The character attacks the opponent with a bolt of lightning",
+    damage: 35
+}
+
+const summon: Ability = {
+    name: "Skeleton Summon",
+    description: "The character summons a skeleton that attacks the enemy and then disappears",
+    damage: 27
+}
