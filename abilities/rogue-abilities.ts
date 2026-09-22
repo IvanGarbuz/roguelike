7e5ad2c37759eb83788fd3cb8b45a7f82b9c99f0
temcha111 <wwwrihydered@gmail.com>
2026-09-22 19:39:21 +0300

    add rogue abilities

---
+++ abilities/rogue-abilities.ts
@@ -0,0 +1,20 @@
+import { Ability } from "./warrior-abilities";
+
+const barrageOfRapidBlows: Ability = {
+    name: "Barrage of rapid blows",
+    description: "The character deals damage to enemies with a combination of several powerful blows",
+    damage: 30
+}
+
+const backStab: Ability = {
+    name: "Back stab",
+    description: "The character deals critical damage by striking the opponent's back",
+    damage: 35
+}
+
+const stealthChokehold: Ability = {
+    name: "Stealth chokehold",
+    description: "A character in stealth mode puts an opponent in a chokehold",
+    damage: 22
+}
+
