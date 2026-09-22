c55d766f83837568e1e770a35e178f5a5d368575
ASDLORE <145283228+younothing5@users.noreply.github.com>
2026-09-22 19:54:47 +0300

    add abilities (#22)
    * add abilities
    
    * add rogue abilities
    
    * add mage abilities
    
    ---------
    
    Co-authored-by: temcha111 <wwwrihydered@gmail.com>

---
+++ abilities/healer-abilities.ts
@@ -0,0 +1,25 @@
+import { Ability } from "./warrior-abilities";
+
+const minorHealing: Ability = {
+    name: "Minor healing",
+    description: "Recites a prayer for a comrade who has fallen into trouble",
+    damage: -15,
+};
+
+const moderateHealing: Ability = {
+    name: "Moderate healing",
+    description: "Calls for sacred prayers for the protection of those close to him",
+    damage: -25,
+};
+
+const massHealing: Ability = {
+    name: "Mass healing",
+    description: "Uses the chronicle to fully heal their team",
+    damage: -20,
+};
+
+const salvation: Ability = {
+    name: "Salvation",
+    description: "Calls upon God for help and protection against all misfortunes",
+    damage: -40,
+};
